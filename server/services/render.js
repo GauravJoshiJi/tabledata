8d33c2b1e86f30b2e8d823055b58e8155620caf5
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2024-01-06 13:04:32 +0530

    showing databse data into frontend

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,14 @@
+const axios = require("axios");
+
 exports.home_route = (req, res) => {
-  res.render("index");
+  axios
+    .get("http://localhost:3000/api/users")
+    .then((response) => {
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.status(200).send({ message: `Error. ${err}` });
+    });
 };
 exports.add_user = (req, res) => {
   res.render("add_user");
