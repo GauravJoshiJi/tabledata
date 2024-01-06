d6f9195e074fd208dc852674334aeccc1311268b
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2024-01-06 12:57:36 +0530

    REST-FULL api completed

---
+++ server/controller/controller.js
@@ -0,0 +1,101 @@
+var Userdb = require("../model/model");
+
+//Create new User
+exports.create = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty" });
+    return;
+  }
+  //new user
+  const user = new Userdb({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    rating: req.body.rating,
+    image: req.body.image,
+  });
+
+  //save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+      //   res.redirect("/add-user");
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occure while creating a create operation",
+      });
+    });
+};
+//Reading data
+exports.find = (req, res) => {
+  if (req.query.id) {
+    const id = req.query.id;
+
+    Userdb.findById(id)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({ message: "Not found user with id " + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Error in retriving user: " + id });
+      });
+  } else {
+    Userdb.find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "Error Occurred while retriving user information",
+        });
+      });
+  }
+};
+
+//Update user
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update can not be empty" });
+  }
+
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `Cannot Update user with ${id}` });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update user information" });
+    });
+};
+
+//Delete user
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Userdb.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `Cannot Delete with id ${id}` });
+      } else {
+        res.send({
+          message: "User was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete ",
+      });
+    });
+};
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const route = express.Router();
 const services = require("../services/render");
+const controller = require("../controller/controller");
 
+// @description: for home page
+// Method: GET
 route.get("/", services.home_route);
+// @description: for adding new user
+// Method: GET
 route.get("/add-user", services.add_user);
+// @description: for updating user
+// Method: GET
 route.get("/update-user", services.update_user);
+
+//RESTFULL API's
+//CREATE
+route.post("/api/users", controller.create);
+//READ
+route.get("/api/users", controller.find);
+//UPDATE
+route.put("/api/users/:id", controller.update);
+//DELETE
+route.delete("/api/users/:id", controller.delete);
 
 module.exports = route;
